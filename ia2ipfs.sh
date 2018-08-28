86660cac8db360096d97480cb7220eff20ba3295
Arkadiy Kukarkin <parkan@users.noreply.github.com>
2018-08-28 16:31:53 -0400

    cleanup

--- ia2ipfs.sh
+++ ia2ipfs.sh
@@ -4,11 +4,12 @@
 
 ID=$1
 FORMAT=${2:-"h.264"}
-LEAFURL=https://gateway.dweb.me/arc/archive.org/leaf/${ID}
-METADATA_PATH=$(curl -s $LEAFURL | jq -r ".[] | fromjson | .urls | .[] | select(startswith(\"ipfs\"))")
+LEAF_URL="https://gateway.dweb.me/arc/archive.org/leaf/${ID}"
+METADATA_PATH=$(curl -s $LEAF_URL | jq -r ".[] | fromjson | .urls | .[] | select(startswith(\"ipfs\"))")
 METADATA_CID=${METADATA_PATH#"ipfs:"}
 echo "metadata CID: ${METADATA_CID}"
 FILENAME=$(ipfs cat ${METADATA_CID} | jq -r ".files | .[] | select(.format==\"${FORMAT}\") | .name")
-MEDIA_PATH=$(curl -s "https://gateway.dweb.me/arc/archive.org/metadata/${ID}/${FILENAME}" | jq -r ".ipfs")
+SEED_URL="https://gateway.dweb.me/arc/archive.org/metadata/${ID}/${FILENAME}"
+MEDIA_PATH=$(curl -s ${SEED_URL} | jq -r ".ipfs")
 MEDIA_CID=${MEDIA_PATH#"ipfs:"}
 echo "media CID: ${MEDIA_CID}"
